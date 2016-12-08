22374f3a4454e94a623cd7f1398152e4159d7d65
xumingze <xumingze@zhumengyuan.com>
2016-12-08 08:49:35 +0800

    update

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
+
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 import os
+
 from setuptools import setup, find_packages
 
+
 here = os.path.abspath(os.path.dirname(__file__))
-readme_file = os.path.join(here, 'README.md')
-changes_file = os.path.join(here, 'CHANGES.md')
+README = open(os.path.join(here, 'Readme.md')).read()
 
-long_description = '\n\n'.join((
-    open(readme_file).read(),
-    open(changes_file).read(),
-))
+requires = [
+    'requests>=2.7',
+]
 
+includes = (
+    'django_callbacks',
+)
 
 setup(
-    name='django_callback',
+    name="sms-provider-python",
     version='0.1.0',
-    url='https://github.com/xumingze2014/django_callbacks',
-    license='BSD',
-    description='channel support for django.',
-    long_description=long_description,
-    author="xumingze",
-    author_email='wsxmz1991@163.com',
     packages=find_packages(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities'
-    ],
-    zip_safe=True,
-    install_requires=['setuptools', ],
+    install_requires=requires,
+    description='django_callbacks',
+    long_description=README,
+    author="zhumengyuan",
+    license="BSD",
+    url=""
 )
